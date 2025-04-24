f80bf007c7265de878cfa563288684e81991eeb6
fahmihasanasagaf <sayafahmi18@gmail.com>
2025-04-25 00:01:17 +0700

    final push update source project

--- TP_modul_7_2311104074/TP_modul_7_2311104074/DataMahasiswa2311104074.cs
+++ TP_modul_7_2311104074/TP_modul_7_2311104074/DataMahasiswa2311104074.cs
@@ -3,6 +3,46 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
 
+public class DataMahasiswa2311104074
+{
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+    public string gender { get; set; }
+    public int age { get; set; }
+    public Address address { get; set; }
+    public List<Course> courses { get; set; }
+
+    public void ReadJSON(string path)
+    {
+        try
+        {
+            string jsonString = File.ReadAllText(path);
+            var data = JsonSerializer.Deserialize<DataMahasiswa2311104074>(jsonString);
+
+            if (data != null)
+            {
+                Console.WriteLine($"Nama: {data.firstName} {data.lastName}");
+                Console.WriteLine($"Gender: {data.gender}");
+                Console.WriteLine($"Age: {data.age}");
+                Console.WriteLine($"Alamat: {data.address.streetAddress}, {data.address.city}, {data.address.state}");
+                Console.WriteLine("Mata Kuliah:");
+                foreach (var course in data.courses)
+                {
+                    Console.WriteLine($"- {course.code}: {course.name}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Data deserialized null");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Terjadi error: {ex.Message}");
+        }
+    }
+}
+
 public class Address
 {
     public string streetAddress { get; set; }
@@ -14,31 +54,4 @@ public class Course
 {
     public string code { get; set; }
     public string name { get; set; }
-}
-
-public class DataMahasiswa2311104074
-{
-    public string firstName { get; set; }
-    public string lastName { get; set; }
-    public string gender { get; set; }
-    public int age { get; set; }
-    public Address address { get; set; }
-    public List<Course> courses { get; set; }
-
-    public static void ReadJSON()
-    {
-        string json = File.ReadAllText("jurnal7_1_2311104074.json");
-        var data = JsonSerializer.Deserialize<DataMahasiswa2311104074>(json);
-
-        Console.WriteLine("=== DATA MAHASISWA ===");
-        Console.WriteLine($"Nama     : {data.firstName} {data.lastName}");
-        Console.WriteLine($"Gender   : {data.gender}");
-        Console.WriteLine($"Umur     : {data.age}");
-        Console.WriteLine($"Alamat   : {data.address.streetAddress}, {data.address.city}, {data.address.state}");
-        Console.WriteLine("Mata Kuliah:");
-        foreach (var course in data.courses)
-        {
-            Console.WriteLine($"- {course.code}: {course.name}");
-        }
-    }
 }
--- TP_modul_7_2311104074/TP_modul_7_2311104074/Program.cs
+++ TP_modul_7_2311104074/TP_modul_7_2311104074/Program.cs
@@ -1,7 +1,11 @@
-﻿class Program
+﻿using System;
+
+class Program
 {
     static void Main(string[] args)
     {
-        DataMahasiswa2311104074.ReadJSON();
+        string path = @"C:\Users\fahmi\modul7_kelompok_1\TP_modul_7_2311104074\TP_modul_7_2311104074\jurnal7_1_2311104074.json";
+        var data = new DataMahasiswa2311104074();
+        data.ReadJSON(path);
     }
 }
