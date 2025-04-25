6184803f8be0becbb5e099b211fa5b47d970d6e9
Ramdaniiiii <168392836+Ramdaniiiii@users.noreply.github.com>
2025-04-25 13:56:32 +0700

    menambahkan class DataMahasiswa2311104044

---
+++ tp7_2311104044/tp7_2311104044/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Memanggil method ReadJSON dari class DataMahasiswa2311104073
+        DataMahasiswa2311104044.ReadJSON();
+
+    }
+}
